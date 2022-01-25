e7ed4e56d49d0d456c79f00244f0e68b0df2d41b
Will <wor101@gmail.com>
2022-01-24 23:22:59 -0500

    adding addtional routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const db = require('./db/index.js')
 
 app.use(cors())
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const { Pool } = require('pg')
 
@@ -17,32 +19,6 @@ const pool = new Pool({
 })
 
 
-let data = [
-  { id: 1,
-    name: "Morgar Firebeard", 
-    ancestory: "Dwarf",
-    class: "Fighter"
-  },
-  {
-    id: 2,
-    name: "Barco the Barbarian",
-    ancestory: "Human",
-    class: "Barbarian"
-  },
-  {
-    id: 3,
-    name: "Theondondandolis",
-    ancestory: "Human",
-    class: "Bard"
-  },
-  {
-    id: 4,
-    name: "Professor Tidwick",
-    ancestory: "Gnome",
-    class: "Rogue"
-  }
-]
-
 
 app.get('/api/data', cors(), (request, response) => {
   pool
@@ -52,7 +28,7 @@ app.get('/api/data', cors(), (request, response) => {
         .query('SELECT * FROM characters')
         .then(res => {
           client.release()
-          console.log(res.rows)
+          // console.log(res.rows)
           response.send(res.rows)
         })
         .catch(err => {
@@ -62,7 +38,34 @@ app.get('/api/data', cors(), (request, response) => {
       })
 })
 
+app.post('/api/data/snack', (request, response) => {
+  const snackName = request.body.name
+  pool.query(`INSERT INTO characters (name) VALUES ('${snackName}');`, (err, res) => {
+    if (err) {
+      throw err
+    }
+    console.log(res.command)
+    console.log(res)
+    console.log(snackName)
+  })  
+})
+
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+})
+
+app.delete('/api/data/snack/:id', (request, response) => {
+  const snackId = request.params.id
+  console.log(snackId)
+  pool.query(`DELETE FROM characters WHERE id=${snackId}`, (err, res) => {
+    if (err) {
+      throw err
+    } else {
+      console.log(res.command)
+      response.sendStatus(200)
+    }
+ })
 })
